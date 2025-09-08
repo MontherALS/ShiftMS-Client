ba7831664c82d8b92b8927d63cce7c61230760c6
MontherALS <montheralshammari31@gmail.com>
2025-09-08 12:18:52 +0300

    adding simple navigation for homepage

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,86 +1,86 @@
-// "use client";
-// import React from "react";
-// import { useState } from "react";
-// import Image from "next/image";
-// import logo from "@/public/Images/logo.png";
-// export default function LoginPage() {
-//   const [formData, setFormData] = useState({
-//     email: "",
-//     password: "",
-//   });
-//   const [message, setMessage] = useState("");
-//   function handleChange(e) {
-//     const { name, value } = e.target;
-//     setFormData((prevData) => ({
-//       ...prevData,
-//       [name]: value,
-//     }));
-//   }
-//   async function handleSubmit(e) {
-//     e.preventDefault();
-//     const res = await fetch("http://localhost:5000/login", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify(formData),
-//     });
-//     if (!res.ok) {
-//       const errorData = await res.json();
-//       setMessage(errorData.message);
-//       return;
-//     }
+"use client";
+import React from "react";
+import { useState } from "react";
+import Image from "next/image";
+import logo from "@/public/Images/logo.png";
+export default function LoginPage() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [message, setMessage] = useState("");
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  }
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const res = await fetch("http://localhost:5000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    if (!res.ok) {
+      const errorData = await res.json();
+      setMessage(errorData.message);
+      return;
+    }
 
-//     const data = await res.json();
+    const data = await res.json();
 
-//     if (data.token) {
-//       localStorage.setItem("token", data.token);
-//       window.location.href = "/dashboard";
-//     }
-//   }
-//   return (
-//     <section className="bg-gradient-to-bl from-blue-50 to-indigo-100 min-h-screen flex items-center justify-center p-4">
-//       <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-md">
-//         <div className=" flex justify-center flex-col text-center items-center mb-6">
-//           <Image alt="logo" width={150} height={150} src={logo} />
-//           <span className="text-red-500">{message}</span>
-//           <h1 className="text-2xl font-bold text-gray-800">Welcome Back</h1>
-//           <p className="text-gray-600 mt-2">Login to accsees the service</p>
-//         </div>
+    if (data.token) {
+      localStorage.setItem("token", data.token);
+      window.location.href = "/dashboard";
+    }
+  }
+  return (
+    <section className="bg-gradient-to-bl from-blue-50 to-indigo-100 min-h-screen flex items-center justify-center p-4">
+      <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-md">
+        <div className=" flex justify-center flex-col text-center items-center mb-6">
+          <Image alt="logo" width={150} height={150} src={logo} />
+          <span className="text-red-500">{message}</span>
+          <h1 className="text-2xl font-bold text-gray-800">Welcome Back</h1>
+          <p className="text-gray-600 mt-2">Login to accsees the service</p>
+        </div>
 
-//         <form onSubmit={handleSubmit} className="space-y-4">
-//           <div>
-//             <input
-//               value={formData.email}
-//               onChange={handleChange}
-//               type="email"
-//               name="email"
-//               placeholder="Email address"
-//               className="w-full p-3 border text-gray-600 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-//               required
-//             />
-//           </div>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <input
+              value={formData.email}
+              onChange={handleChange}
+              type="email"
+              name="email"
+              placeholder="Email address"
+              className="w-full p-3 border text-gray-600 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              required
+            />
+          </div>
 
-//           <div>
-//             <input
-//               value={formData.password}
-//               onChange={handleChange}
-//               type="password"
-//               name="password"
-//               placeholder="Password"
-//               className="w-full p-3 border text-gray-600 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-//               required
-//             />
-//           </div>
+          <div>
+            <input
+              value={formData.password}
+              onChange={handleChange}
+              type="password"
+              name="password"
+              placeholder="Password"
+              className="w-full p-3 border text-gray-600 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              required
+            />
+          </div>
 
-//           <button
-//             type="submit"
-//             className="w-full bg-blue-600 text-white p-3 rounded-lg hover:bg-blue-700 transition duration-200"
-//           >
-//             Sign In
-//           </button>
-//         </form>
-//       </div>
-//     </section>
-//   );
-// }
+          <button
+            type="submit"
+            className="w-full bg-blue-600 text-white p-3 rounded-lg hover:bg-blue-700 transition duration-200"
+          >
+            Sign In
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -3,29 +3,42 @@ import Link from "next/link";
 import Image from "next/image";
 import logo from "@/public/Images/logo.png";
 export default function Home() {
+  const navigationsTaps = [{
+    name:"Home",
+    url: "/"
+  },
+{
+    name:"Product",
+    url: "/product"
+  },
+{
+    name:"About us",
+    url: "/about-us"
+  }]
   return (
-    <main className="min-h-screen bg-gradient-to-bl from-blue-50 to-indigo-100 flex items-center justify-center px-6">
-      <div className="max-w-3xl flex flex-col items-center text-center space-y-6">
-        <Image src={logo} width={175} alt="logo" />
-        <h1 className="text-4xl sm:text-5xl font-bold text-gray-800">
-          <span className="text-indigo-800 font-serif">
-            Shift Management System
-          </span>
-        </h1>
-
-        <p className="text-lg sm:text-xl text-gray-600 font-sans">
-          A smart platform for managing employee shifts and group schedules with
-          ease and clarity. Save time, stay organized, and keep everything in
-          one place.
-        </p>
-
-        <Link
-          href="/dashboard"
-          className="inline-block mt-4 bg-orange-400 text-white text-lg font-medium px-6 py-3 rounded-lg shadow hover:bg-orange-600 duration-500 transition"
-        >
-          <span className="text-white">Get Started </span>
-        </Link>
+    <main className="min-h-screen bg-gradient-to-bl from-blue-50 to-indigo-100 flex flex-col">
+      
+      <nav className=" text-black flex justify-between items-center bg-gradient-to-l from-blue-100 to-stone-300 px-2">
+      <div className="flex gap-2 items-center justify-center">
+        <Image src={logo} width={75} height={75} alt="logo"/>
+        <h1 className="text-2xl">SMS</h1>
       </div>
+      <div className="flex gap-5">  
+   {navigationsTaps.map((t,i)=> 
+   <Link key={i} href={t.url} className="text-gray-700 text-[1.2rem] hover:text-[1.3rem] transform duration-500 hover:text-gray-950">
+   {t.name}
+   </Link>
+  )}
+  </div>
+    
+    <div>
+      <Link href="/login" className="text-[1.4rem] bg-white/50 text-gray-700  font-serif rounded-full hover:bg-white border  duration-500 px-4 py-2">
+    Log in
+    </Link>
+    </div>
+
+    </nav>
+       <header className="text-gray-600"></header>
     </main>
   );
 }
